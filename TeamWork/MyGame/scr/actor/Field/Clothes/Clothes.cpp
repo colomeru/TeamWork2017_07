ff32f1fb41660fffdad6a10177d6d9ba331ee5dc
sugi23 <sugi23l4game@outlook.jp>
2017-04-20 14:36:04 +0900

    レーンナンバー追加

--- TeamWork/MyGame/scr/actor/Field/Clothes/Clothes.cpp
+++ TeamWork/MyGame/scr/actor/Field/Clothes/Clothes.cpp
@@ -1,7 +1,7 @@
 #include "Clothes.h"
 
 //コンストラクタ
-Clothes::Clothes(IWorld* world, CLOTHES_ID clothes)
+Clothes::Clothes(IWorld* world, CLOTHES_ID clothes, int laneNum)
 	:Actor(world)
 	,isHit_(false)
 {

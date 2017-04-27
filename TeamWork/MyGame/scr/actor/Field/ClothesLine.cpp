e328bb5d42a0acbea8b64b0e7c36e61e44753cf4
DESKTOP-CMJAC51\ndsks <microbug7@gmail.com>
2017-04-27 16:24:29 +0900

    レーンの描画

---
+++ TeamWork/MyGame/scr/actor/Field/ClothesLine.cpp
@@ -0,0 +1,91 @@
+#include "ClothesLine.h"
+#include "../MyGame/scr/graphic/Sprite.h"
+#include "../MyGame/scr/input/Keyboard.h"
+
+ClothesLine::ClothesLine(IWorld * world, int laneNum, Vector2 pos)
+	:Actor(world)
+{
+	parameter_.ID = ACTOR_ID::UI_ACTOR;
+	parameter_.radius = 32.0f;
+	parameter_.size = Vector2(200, 50.f);
+	parameter_.mat
+		= Matrix::CreateScale(Vector3::One)
+		* Matrix::CreateRotationZ(0.0f)
+		* Matrix::CreateTranslation(Vector3(0, 0, 0));
+
+	laneNum_ = laneNum;
+	position_ = pos;
+
+}
+
+ClothesLine::~ClothesLine()
+{
+}
+
+void ClothesLine::Update()
+{
+	//ShakesClothes();
+
+}
+
+void ClothesLine::Draw() const
+{
+	auto is = Matrix::CreateRotationZ(angle_);
+	auto pos = drawPos_;
+	auto sizeVec = Vector3((parameter_.size.x / 2), (parameter_.size.y / 2));
+
+	auto box1 = Vector3(-sizeVec.x, -sizeVec.y)*is;
+	auto box2 = Vector3(+sizeVec.x, -sizeVec.y)*is;
+	auto box3 = Vector3(-sizeVec.x, +sizeVec.y)*is;
+	auto box4 = Vector3(+sizeVec.x, +sizeVec.y)*is;
+	//auto seg = Vector3(+sizeVec.x, 0)*is;
+	//ç∂è„,âEè„,ç∂â∫,âEâ∫
+	auto pos1 = Vector3(pos.x + box1.x, pos.y + box1.y);
+	auto pos2 = Vector3(pos.x + box2.x, pos.y + box2.y);
+	auto pos3 = Vector3(pos.x + box3.x, pos.y + box3.y);
+	auto pos4 = Vector3(pos.x + box4.x, pos.y + box4.y);
+	//Model::GetInstance().Draw(MODEL_ID::PLAYER_MODEL, parameter_.mat);
+	DrawLine(pos1.x, pos1.y, pos2.x, pos2.y, GetColor(255, 255, 255));
+	DrawLine(pos1.x, pos1.y, pos3.x, pos3.y, GetColor(255, 255, 255));
+	DrawLine(pos2.x, pos2.y, pos4.x, pos4.y, GetColor(255, 255, 255));
+	DrawLine(pos3.x, pos3.y, pos4.x, pos4.y, GetColor(255, 255, 255));
+
+	DrawBox(pos1.x, pos1.y, pos4.x, pos4.y, GetColor(255, 255, 0), TRUE);
+}
+
+void ClothesLine::OnUpdate()
+{
+}
+
+void ClothesLine::OnCollide(Actor * other, CollisionParameter colpara)
+{
+}
+
+void ClothesLine::OnMessage(EventMessage message, void * param)
+{
+	//switch (message)
+	//{
+	//case EventMessage::BEGIN_WIND:
+	//	basePosition_ = position_;
+	//	isPendulum_ = true;
+	//	break;
+	//case EventMessage::STRONG_WIND:
+	//	rot_spd_ = 2.8f;
+	//	isWind_ = true;
+	//	break;
+	//case EventMessage::ATTENUATE_WIND:
+	//	rot_spd_ = 0.0f;
+	//	isFriction_ = true;
+	//	break;
+	//case EventMessage::END_WIND:
+	//	rot_spd_ = 0.5f;
+	//	rot_ = 90.0f;
+	//	friction_ = 1.0f;
+	//	angle_ = 0;
+	//	position_ = basePosition_;
+	//	isPendulum_ = false;
+	//	isFriction_ = false;
+	//	isWind_ = false;
+	//	break;
+	//}
+}
---
+++ TeamWork/MyGame/scr/actor/Field/ClothesLine.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "../Actor.h"
+
+class ClothesLine : public Actor, public std::enable_shared_from_this<Actor>
+{
+public:
+	//コンストラクタ
+	ClothesLine(IWorld* world, int laneNum, Vector2 pos);
+	//デストラクタ
+	~ClothesLine();
+	//更新
+	virtual void Update() override;
+	//描画
+	virtual void Draw() const override;
+	//受動更新
+	virtual void OnUpdate() override;
+	//衝突時のアクション
+	virtual void OnCollide(Actor* other, CollisionParameter colpara);
+	//メッセージ取得
+	virtual void OnMessage(EventMessage message, void* param);
+
+private:
+};

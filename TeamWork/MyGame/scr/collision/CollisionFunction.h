96f5071b15d0ce67f713d129ec9370f82aa0f823
colomeru <myongin422@gmail.com>
2017-04-27 10:15:48 +0900

    当たり判定部分の切り分け

---
+++ TeamWork/MyGame/scr/collision/CollisionFunction.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class IWorld;
+class Actor;
+struct CollisionParameter;
+
+class CollisionFunction
+{
+public:
+	// コンストラクタ
+	explicit CollisionFunction(IWorld* world); 
+
+	// スプライトの当たり判定（２次元のOBB vs OBB）
+	CollisionParameter IsHit_OBB_OBB(const Actor& sprite1, const Actor& sprite2);
+	// スプライトの当たり判定（２次元のOBB vs 線分）
+	CollisionParameter IsHit_OBB_Segment(const Actor& sprite1, const Actor& sprite2);
+	// スプライトの当たり判定（２次元の線分 vs 線分）
+	CollisionParameter IsHit_Segment_Segment(const Actor& sprite1, const Actor& sprite2);
+	// スプライトの当たり判定（２次元の円 vs 円）
+	CollisionParameter IsHit_Circle_Circle(const Actor& sprite1, const Actor& sprite2);
+	// スプライトの当たり判定（２次元の円 vs 線分）
+	CollisionParameter IsHit_Circle_Segment(const Actor& sprite1, const Actor& sprite2);
+	// スプライトの当たり判定（２次元のOBB vs 円）
+	CollisionParameter IsHit_OBB_Circle(const Actor& sprite1, const Actor& sprite2);
+	// スプライトの当たり判定（２次元のOBB vs 服）
+	CollisionParameter IsHit_OBB_Clothes(const Actor& sprite1, const Actor& sprite2);
+	CollisionParameter IsHit_Clothes_OBB(const Actor& sprite1, const Actor& sprite2);
+
+
+private:
+	// ワールド
+	IWorld*	world_;
+};

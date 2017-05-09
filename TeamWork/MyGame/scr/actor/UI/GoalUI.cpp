ffaa8e4145e0a920b1987b117f51cc08d3bdd6a2
sugi23 <sugi23l4game@outlook.jp>
2017-05-09 16:46:35 +0900

    ゴールの修正

--- TeamWork/MyGame/scr/actor/UI/GoalUI.cpp
+++ TeamWork/MyGame/scr/actor/UI/GoalUI.cpp
@@ -1,4 +1,5 @@
 #include "GoalUI.h"
+#include "../../Def.h"
 
 GoalUI::GoalUI(IWorld * world, Vector2 pos)
 	:Actor(world)
@@ -18,7 +19,7 @@ void GoalUI::Update()
 void GoalUI::Draw() const
 {
 	Vector2 crcOrigin = Sprite::GetInstance().GetSize(SPRITE_ID::TEST_GOAL_SPRITE) / 2;
-	Sprite::GetInstance().Draw(SPRITE_ID::TEST_GOAL_SPRITE, Vector2(drawPos_.x, 500), crcOrigin, 1.0f, Vector2::One);
+	Sprite::GetInstance().Draw(SPRITE_ID::TEST_GOAL_SPRITE, Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), crcOrigin, 1.0f, Vector2::One);
 }
 
 void GoalUI::OnUpdate()

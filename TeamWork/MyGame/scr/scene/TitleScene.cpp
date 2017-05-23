2425b73429e9368e3ce62e60f601a8654e55e7ae
DESKTOP-DGLO0UK\sth46 <sth4628@hotmail.com>
2017-05-23 13:40:43 +0900

    Title まとめ

--- TeamWork/MyGame/scr/scene/TitleScene.cpp
+++ TeamWork/MyGame/scr/scene/TitleScene.cpp
@@ -43,7 +43,25 @@ void TitleScene::Update()
 	Camera::GetInstance().Position.Set(Vector3 (0,0,-50));
 	Camera::GetInstance().Target.Set(Vector3(0,0,0));
 	Camera::GetInstance().Update();
+	if (Keyboard::GetInstance().KeyTriggerDown(KEYCODE::UP)) {
+		selectX_ = 850.0f;
+		selectY_ = 703.0f;
+		selectNum_ = 0;
+	}
+	if (Keyboard::GetInstance().KeyTriggerDown(KEYCODE::DOWN)) {
+		selectX_ = 850.0f;
+		selectY_ = 768.0f;
+		selectNum_ = 1;
+	}
 
+	if (Keyboard::GetInstance().KeyTriggerDown(KEYCODE::SPACE)) {
+		if (selectNum_ == 0) {
+			isEnd_ = true;
+		}
+		else if (selectNum_ == 1) {
+			Escape;
+		}
+	}
 }
 
 void TitleScene::Draw() const
@@ -52,8 +70,14 @@ void TitleScene::Draw() const
 	// 読みこんだグラフィックを画面左上に描画
 	//auto pos1 = DXConverter::GetInstance().ToVECTOR(Vector3(0, 0, 0));
 	//auto pos2 = DXConverter::GetInstance().ToVECTOR(Vector3(0, 0, 0));
-	DrawFormatString(100, 100, GetColor(255, 255, 255), "TitleScene");
-	Sprite::GetInstance().Draw(SPRITE_ID::TITLE_SPRITE, Vector2(0.0f, 0.0f));
+	//DrawFormatString(100, 100, GetColor(255, 255, 255), "TitleScene");
+	auto drawpos = Vector2(WINDOW_WIDTH, WINDOW_HEIGHT) / 2;
+	auto origin = Sprite::GetInstance().GetSize(SPRITE_ID::TITLE_SPRITE) / 2;
+	Sprite::GetInstance().Draw(SPRITE_ID::TITLE_SPRITE, drawpos, origin, Vector2::One);
+	Sprite::GetInstance().Draw(SPRITE_ID::TITLE_START_SPRITE, Vector2(905.0f, 680.0f));
+	Sprite::GetInstance().Draw(SPRITE_ID::TITLE_EXIT_SPRITE, Vector2(905.0f, 740.0f));
+	Sprite::GetInstance().Draw(SPRITE_ID::SPHERE_SPRITE, Vector2(selectX_, selectY_));
+
 	//DrawFormatString(0, 20, GetColor(255, 255, 255), "FPS:[%.1f]", FPS::GetFPS);
 	//DrawCapsule3D(pos1, pos2, 4.0f, 8, GetColor(255, 255, 0), GetColor(255, 255, 0), FALSE);
 	// •`‰æ

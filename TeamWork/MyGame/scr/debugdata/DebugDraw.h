40ac4ae8bb5a6308a39721ea5f17bdfde794c812
DESKTOP-CMJAC51\ndsks <microbug7@gmail.com>
2017-07-14 09:52:44 +0900

    デバッグに四角形描画追加

--- TeamWork/MyGame/scr/debugdata/DebugDraw.h
+++ TeamWork/MyGame/scr/debugdata/DebugDraw.h
@@ -11,6 +11,7 @@ public:
 
 	static void DebugDrawLine(int x1, int y1, int x2, int y2, unsigned int color, int thickness = 1) {}
 	static void DebugDrawCircle(int x, int y, int r, unsigned int color, int FillFlag=1, int LineThickness = 1) {}
+	static void DebugDrawBox(int x1, int y1, int x2, int y2, unsigned int color, int FillFlag) {}
 
 #else
 	template<class ...T>
@@ -22,10 +23,15 @@ public:
 	static void DebugDrawLine(int x1, int y1, int x2, int y2, unsigned int color, int thickness = 1) {
 		DrawLine(x1, y1, x2, y2, color, thickness);
 	}
-	//デバッグ用線分描画
-	static void DebugDrawCircle(int x, int y, int r, unsigned int color,int FillFlag=1, int LineThickness = 1) {
-		DrawCircle(x, y,r, color,FillFlag,LineThickness);
+	//デバッグ用円描画
+	static void DebugDrawCircle(int x, int y, int r, unsigned int color, int FillFlag = 1, int LineThickness = 1) {
+		DrawCircle(x, y, r, color, FillFlag, LineThickness);
 	}
+	//デバッグ用矩形描画
+	static void DebugDrawBox(int x1, int y1, int x2, int y2,unsigned int color, int FillFlag) {
+		DrawBox(x1, y1,x2,y2,color, FillFlag);
+	}
+
 
 #endif
 
